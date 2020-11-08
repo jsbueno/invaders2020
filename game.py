35162ff22a0201744b0663fb04ef24b8bec540a9
Joao S O Bueno <gwidion@gmail.com>
2020-11-08 14:07:48 -0300

    Nave inimiga, sem classes

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 
-TAMANHO = (640, 480)
+TAMANHO = (800, 600)
 LARGURA, ALTURA = TAMANHO
 tela = None
 
@@ -9,6 +9,10 @@ tamanho_nave = 64
 x = LARGURA // 2
 y = ALTURA - tamanho_nave
 
+x_i = 0
+y_i = tamanho_nave
+cont_i = 0
+
 def inicio():
     global tela
     tela = pygame.display.set_mode(TAMANHO)
@@ -16,22 +20,33 @@ def inicio():
 def atualiza():
     global x, y
     teclas = pygame.key.get_pressed()
-    if teclas[pygame.K_LEFT] and x > 0:
+    if teclas[pygame.K_LEFT]:
         x -= tamanho_nave // 2
-    if teclas[pygame.K_RIGHT] and x < (LARGURA - tamanho_nave):
+    if teclas[pygame.K_RIGHT]:
         x += tamanho_nave // 2
-
+    if x < 0:
+        x = 0
+    elif x + tamanho_nave > LARGURA:
+        x = LARGURA - tamanho_nave
 
 def desenha():
-    global x, y, tamanho_nave
-    tela.fill((0, 0, 0))
     pygame.draw.rect(tela, (0, 255, 0), (x, y, tamanho_nave, tamanho_nave))
+
+def atualiza_i():
+    global x_i, y_i, cont_i
+    if cont_i % 4 == 0:
+        x_i += tamanho_nave // 2
+        if x_i + tamanho_nave > LARGURA:
+            x_i = 0
+    cont_i += 1
+
+def desenha_i():
+    pygame.draw.rect(tela, (192, 0, 0), (x_i, y_i, tamanho_nave, tamanho_nave))
 
 def principal():
 
     fim_de_jogo = False
     while not fim_de_jogo:
-        desenha()
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT or evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
@@ -39,6 +54,11 @@ def principal():
             #if evento.type == pygame.KEYDOWN:
             #    atualiza(evento)
         atualiza()
+        atualiza_i()
+        tela.fill((0, 0, 0))
+        desenha()
+        desenha_i()
+
         pygame.display.flip()
         pygame.event.pump()
         pygame.time.delay(30)
