9db8231fa95183eab19df06b42e05c80d53c9053
Joao S O Bueno <gwidion@gmail.com>
2020-11-08 12:39:56 -0300

    Evento de fechar

--- game.py
+++ game.py
@@ -11,11 +11,18 @@ def desenha():
     pygame.draw.rect(tela, (0, 255, 0), (50, 50, 200, 150))
 
 def principal():
-    desenha()
-    for i in range(100):
-        pygame.event.pump()
+
+    fim_de_jogo = False
+    while not fim_de_jogo:
+        desenha()
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT or evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                fim_de_jogo = True
+
         pygame.display.flip()
-        pygame.time.delay(100)
+        pygame.event.pump()
+        pygame.time.delay(30)
 
 inicio()
 principal()
